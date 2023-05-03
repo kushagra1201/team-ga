23ccced86e2868c1a43d50ce63f0f2414ab0401f
kushagra1201 <imt_2019053@iiitm.ac.in>
2023-05-03 13:27:07 +0530

    added global population, fitness calculation and selection functions

--- main.cpp
+++ main.cpp
@@ -1,21 +1,83 @@
 #include <bits/stdc++.h>
-int features = 4;
 using namespace std;
 
-vector<vector<int>> gen_chromosomes()
+bool cmp(const vector<int> &a, const vector<int> &b)
+{
+    return a[11] > b[11];
+}
+
+vector<vector<int>> global_population(20);
+
+void generate_global()
+{
+
+    for (int i = 0; i < global_population.size(); i++)
+    {
+        vector<int> v;
+        for (int j = 0; j < 11; j++)
+        {
+            v.push_back(1 + rand() % 100);
+        }
+        global_population[i] = v;
+    }
+
+    return;
+}
+
+vector<vector<int>> generate_chromosomes()
 {
     // generate random population (chromosomes)
-    // consider each feature range and generate random values into a vector
-    vector<vector<int>> pop(11, vector<int>(features, 0));
-    for (int i = 0; i < 11; i++)
+
+    // take random indices from the global population
+    vector<int> indices(10, 0);
+    for (int i = 0; i < indices.size(); i++)
     {
-        for (int j = 0; j < features; j++)
+        indices[i] = rand() % 10;
+    }
+
+    vector<int> indices_not_there;
+    for (int i = 0; i < global_population.size() - indices.size(); i++)
+    {
+        auto pos = find(indices.begin(), indices.end(), i);
+        if (pos == indices.end())
         {
-            pop[i][j] = 1 + rand() % 100;
+            indices_not_there.push_back(i);
         }
     }
 
-    return pop;
+    vector<vector<int>> chromosomes;
+    for (int i = 0; i < indices.size(); i++)
+    {
+        chromosomes.push_back(global_population[indices[i]]);
+    }
+
+    return chromosomes;
+}
+
+vector<int> calculate_fitness(vector<vector<int>> population_chromosomes)
+{
+
+    vector<int> fitness;
+    for (int i = 0; i < population_chromosomes.size(); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < population_chromosomes[0].size(); j++)
+        {
+            sum += population_chromosomes[i][j];
+        }
+        fitness.push_back(sum);
+    }
+
+    return fitness;
+}
+
+vector<vector<int>> selection(vector<vector<int>> population_chromosomes, vector<int> fitness)
+{
+    int first = 0, second = 0;
+
+    sort(population_chromosomes.begin(), population_chromosomes.end(), cmp);
+
+    return population_chromosomes;
 }
 
 int main()
@@ -24,10 +86,30 @@ int main()
     cout << "Enter number of generations: ";
     cin >> generations;
 
-    // obtain the population of chromosomes
-    vector<vector<int>> population = gen_chromosomes();
+    // create global population
+    generate_global();
 
-    // reproduction operation on the given population
+    // obtain the population of chromosomes
+    vector<vector<int>> population_chromosomes = generate_chromosomes();
+    vector<int> fitness = calculate_fitness(population_chromosomes);
+
+    for (int i = 0; i < population_chromosomes.size(); i++)
+    {
+        population_chromosomes[i].push_back(fitness[i]);
+    }
+
+    // for (int i = 0; i < population_chromosomes.size(); i++)
+    // {
+    //     for (int j = 0; j < population_chromosomes[0].size(); j++)
+    //     {
+    //         cout << population_chromosomes[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // selection operation on the given population
+    population_chromosomes = selection(population_chromosomes, fitness);
+    // top two solutions would be parents
 
     // selecting best chromosomes as parents
 
